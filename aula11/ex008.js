409cb80af4f49531dd024b7590a9ee96f0b5b0de
lipeezzz <divfelipee@gmail.com>
2025-12-03 18:32:05 -0300

    add

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`A velocidade do seu veículo é ${vel} km/h`)
+if (vel > 60 ) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+} else {
+    console.log(`Você está dentro do permitido.`)
+}
+console.log(`Dirija sempre usando o cinto de segurança.`)
