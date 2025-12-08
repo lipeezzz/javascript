6e4745a243535036310db64d2feec315b646be2b
lipeezzz <divfelipee@gmail.com>
2025-12-08 18:31:02 -0300

    att feita

---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 18
+if (idade < 16) {
+    console.log(`Não pode votar`)
+} else if (idade < 18) {
+        console.log(`Voto opicional`)
+    } else if (idade >= 18) {
+        console.log(`Voto obrigatório`)
+    }
+    
---
+++ aula12/ex012.js
@@ -0,0 +1,14 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são ${hora} horas`)
+if (hora >= 6 && hora < 12) {
+    console.log(`Bom dia`)
+} else if (hora >= 12 && hora < 18) {
+    console.log(`Boa tarde`)
+} else if (hora >= 18 && hora < 24) {
+    console.log(`Boa noite`)
+} else if (hora = 24) {
+    console.log(`Boa madrugada`)
+} else if (hora < 6) {
+    console.log(`Boa madrugada`)
+}
