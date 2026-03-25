bdb9ab243e6c323fbb2df8eb3ace3e17d1857dd6
lipeezzz <divfelipee@gmail.com>
2026-03-25 19:12:26 -0300

    mais uma att

---
+++ aula13/ambiente.js
@@ -0,0 +1,25 @@
+/*
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+console.log("tudo bem?")
+*/
+
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+/*---------------------------------------*/ 
+
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 16)
