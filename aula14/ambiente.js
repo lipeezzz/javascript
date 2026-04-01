0e995598ebe201291e9f15d926eeb264794ecb12
lipeezzz <divfelipee@gmail.com>
2026-04-01 17:57:05 -0300

    att

---
+++ aula14/ambiente.js
@@ -0,0 +1,35 @@
+var c = 1
+
+/*while (c <= 9) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+//---------------------------------
+
+for (var c = 1; c <= 19; c++) {
+    console.log(`passo ${c}`)
+}
+
+//---------------------------------
+
+var x = 1
+while (x <= 20) {
+    console.log(`passo ${x}`)
+    x++
+}`
+
+//--------------------------------- 
+
+for (var y = 1; y <= 12; y++) {
+    console.log(`passo ${y}`)
+}
+*/
+
+//---------------------------------
+
+console.log('Vai começar!!!!')
+for (var b = 1; b <= 20; b++) {
+    console.log(`passo ${b}`)
+}
+console.log('FIM!')
